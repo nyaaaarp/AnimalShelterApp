c523683d23202afe41944ceeb79eb65d12149623
Jo.Camiguing <jo.camiguing@pointwest.com.ph>
2017-11-21 19:44:15 +0800

    Added more files

---
+++ src/com/pointwest/training/bean/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+
+private int employeeId;
+
+public Employee(){
+this.employeeId = 1;
+}
+
+public void setEmployeeId(int employeeId){
+this.employeeId = employeeId;
+}
+
+public int getEmployeeId(){
+return this.employeeId;
+}
